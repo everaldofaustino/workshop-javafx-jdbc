112ec941db8efbbd62e4c7c3635bafdf0c50d7e4
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-26 11:40:54 -0300

    Main view

--- src/main/java/org/everaldo/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/org/everaldo/workshopjavafxjdbc/HelloApplication.java
@@ -2,6 +2,7 @@ package org.everaldo.workshopjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,11 +10,16 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
