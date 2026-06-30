00f3f08fe387ad810d8bf09ebd54f2955e12ae7f
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-30 12:07:26 -0300

    Main view controller

---
+++ src/main/java/org/everaldo/workshopjavafxjdbc/MainViewController.java
@@ -0,0 +1,42 @@
+package org.everaldo.workshopjavafxjdbc;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    private void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    private void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    private void onmenuItemAboutAction(){
+        System.out.println("onmenuItemAboutAction");
+    }
+
+
+
+    @Override
+    public void initialize(URL uri, ResourceBundle resourceBundle) {
+
+    }
+}
