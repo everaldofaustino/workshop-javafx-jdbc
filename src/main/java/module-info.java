67c1f2bdf1908795287aeba719ad86aafff92517
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-30 17:58:14 -0300

    About view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.everaldo.workshopjavafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jdk.compiler;
 
 
     opens org.everaldo.workshopjavafxjdbc to javafx.fxml;
--- src/main/java/org/everaldo/workshopjavafxjdbc/MainViewController.java
+++ src/main/java/org/everaldo/workshopjavafxjdbc/MainViewController.java
@@ -1,9 +1,18 @@
 package org.everaldo.workshopjavafxjdbc;
 
+import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import util.Alerts;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,13 +39,33 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onmenuItemAboutAction(){
-        System.out.println("onmenuItemAboutAction");
+        loadView("/gui/About.fxml");
     }
 
 
 
     @Override
     public void initialize(URL uri, ResourceBundle resourceBundle) {
+
+    }
+
+    private void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = HelloApplication.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVbox.getChildren().get(0);
+            mainVbox.getChildren().clear();
+            mainVbox.getChildren().add(mainMenu);
+            mainVbox.getChildren().addAll(newVBox.getChildren());
+
+        }
+        catch (IOException e){
+            Alerts.showAlert("IO Exception" , "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
 
     }
 }
