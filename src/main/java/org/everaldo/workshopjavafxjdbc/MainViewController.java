bad173038759b65b2cd5140ea631bc17757ece58
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-30 21:15:39 -0300

    Initializing action as parameter

--- src/main/java/org/everaldo/workshopjavafxjdbc/MainViewController.java
+++ src/main/java/org/everaldo/workshopjavafxjdbc/MainViewController.java
@@ -16,6 +16,7 @@ import util.Alerts;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void onMenuItemDepartmentAction(){
-        loadView2("/gui/DepartmentList.fxml");
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
     }
 
     @FXML
     private void onmenuItemAboutAction(){
-        loadView("/gui/About.fxml");
+        loadView("/gui/About.fxml", x -> {});
     }
 
 
@@ -50,7 +54,7 @@ public class MainViewController implements Initializable {
 
     }
 
-    private void loadView(String absoluteName){
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction){
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVBox = loader.load();
@@ -62,6 +66,9 @@ public class MainViewController implements Initializable {
             mainVbox.getChildren().clear();
             mainVbox.getChildren().add(mainMenu);
             mainVbox.getChildren().addAll(newVBox.getChildren());
+
+            T controller = loader.getController();
+            initializingAction.accept(controller);
 
         }
         catch (IOException e){
